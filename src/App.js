026a51e56165df96ca40e06ce0fd095092dbeb47
Git-I985 <chateauamboise15@gmail.com>
2023-02-08 00:16:28 +0300

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './style.scss';
 
-function Input({ label, ...params }) {
+function Input({
+  label,
+  style = {},
+  leftIconClick = () => {},
+  rightIconClick = () => {},
+  ...params
+}) {
   const id = React.useId();
   return (
-    <div class="input-container">
+    <div className="input-container" style={style}>
+      <div className="left-icon" onClick={leftIconClick}></div>
+      <div className="right-icon" onClick={rightIconClick}></div>
       <input id={id} {...{ ...params, placeholder: 'empty' }} />
       <label for={id}>{label}</label>
     </div>
@@ -12,11 +20,38 @@ function Input({ label, ...params }) {
 }
 
 export default function App() {
+  const [val, setVal] = React.useState('');
   return (
     <div class="preview">
       <Input label="Default" />
       <Input label="Disabled" disabled />
       <Input label="Email" type="email" />
+      <Input label="Email invalid prefill" type="email" value="asd" />
+      <Input
+        label="Search"
+        type="text"
+        style={{
+          '--l-icon-size': '20px',
+          '--r-icon-size': '16px',
+          '--icon-space': '4px',
+          '--left-icon': "url('/icons/search.svg')",
+          '--right-icon': "url('/icons/cross.svg')",
+        }}
+        rightIconClick={() => {
+          setVal('');
+        }}
+        leftIconClick={() => {}}
+        value={val}
+        onChange={(e) => setVal(e.target.value)}
+      />
+      <Input
+        label="Search"
+        type="text"
+        style={{
+          '--r-icon-size': '16px',
+          '--right-icon': "url('/icons/cross.svg')",
+        }}
+      />
     </div>
   );
 }
